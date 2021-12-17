84a96f5f6d13e17e5021b14dac6ea301e3ff56f1
Mohamed <31968872+Mohamedkone@users.noreply.github.com>
2021-12-17 15:51:50 -0500

    update section and promotion finished

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,7 +1,64 @@
 import React from "react";
+import "../style/Home.css"
+import Thumbnail from "../assets/kimetsu.jpg"
 
 const Home = () =>{
-    return <p></p>
+    return (
+        <main>
+            <section className="promotion"></section>
+            <section className="sec1 updates">
+                <h1>Updated ...</h1>
+                <div className="container1">
+                    <div className="card">
+                        <img src={Thumbnail} alt="" />
+                        <div className="card-info">
+                            <h3>Lorem, ipsum.</h3>
+                            <h4 className="author">Lorem, ipsum.</h4>
+                            <p className="card-desc">Lorem ipsum dolor sit amet, 
+                                consectetur adipisicing elit. 
+                                Sunt nesciunt fuga molestias corporis 
+                                fugit culpa obcaecati nobis voluptate 
+                                earum voluptatibus!
+                            </p>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <img src={Thumbnail} alt="" />
+                        <div className="card-info">
+                            <h3>Lorem, ipsum.</h3>
+                            <h4 className="author">Lorem, ipsum.</h4>
+                            <p className="card-desc">Lorem ipsum dolor sit amet, 
+                                consectetur adipisicing elit. 
+                                Sunt nesciunt fuga molestias corporis 
+                                fugit culpa obcaecati nobis voluptate 
+                                earum voluptatibus!
+                            </p>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <img src={Thumbnail} alt="" />
+                        <div className="card-info">
+                            <h3>Lorem, ipsum.</h3>
+                            <h4 className="author">Lorem, ipsum.</h4>
+                            <p className="card-desc">Lorem ipsum dolor sit amet, 
+                                consectetur adipisicing elit. 
+                                Sunt nesciunt fuga molestias corporis 
+                                fugit culpa obcaecati nobis voluptate 
+                                earum voluptatibus!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="sec2 suggestions">
+               
+            </section>
+            <section className="sec1 ranks"></section>
+            <section className="sec2 fanArt"></section>
+            <section className="sec1 Manga"></section>
+            <section className="sec2 Manhwa"></section>
+        </main>
+    )
 }
 
 export default Home
