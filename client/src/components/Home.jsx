2b5712931f8e4c8b532678327f78abf03f79ec76
Mohamed <31968872+Mohamedkone@users.noreply.github.com>
2021-12-17 15:54:36 -0500

    suggestion section completed

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -51,7 +51,48 @@ const Home = () =>{
                 </div>
             </section>
             <section className="sec2 suggestions">
-               
+                <h1>Suggestions</h1>
+                <div className="container1">
+                    <div className="card">
+                        <img src={Thumbnail} alt="" />
+                        <div className="card-info">
+                            <h3>Lorem, ipsum.</h3>
+                            <h4 className="author">Lorem, ipsum.</h4>
+                            <p className="card-desc">Lorem ipsum dolor sit amet, 
+                                consectetur adipisicing elit. 
+                                Sunt nesciunt fuga molestias corporis 
+                                fugit culpa obcaecati nobis voluptate 
+                                earum voluptatibus!
+                            </p>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <img src={Thumbnail} alt="" />
+                        <div className="card-info">
+                            <h3>Lorem, ipsum.</h3>
+                            <h4 className="author">Lorem, ipsum.</h4>
+                            <p className="card-desc">Lorem ipsum dolor sit amet, 
+                                consectetur adipisicing elit. 
+                                Sunt nesciunt fuga molestias corporis 
+                                fugit culpa obcaecati nobis voluptate 
+                                earum voluptatibus!
+                            </p>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <img src={Thumbnail} alt="" />
+                        <div className="card-info">
+                            <h3>Lorem, ipsum.</h3>
+                            <h4 className="author">Lorem, ipsum.</h4>
+                            <p className="card-desc">Lorem ipsum dolor sit amet, 
+                                consectetur adipisicing elit. 
+                                Sunt nesciunt fuga molestias corporis 
+                                fugit culpa obcaecati nobis voluptate 
+                                earum voluptatibus!
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </section>
             <section className="sec1 ranks"></section>
             <section className="sec2 fanArt"></section>
