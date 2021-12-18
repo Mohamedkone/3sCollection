1fdd3b3e632102170737752449056325c7c95161
Mohamed <31968872+Mohamedkone@users.noreply.github.com>
2021-12-18 11:41:19 -0500

    basic front end set up for page change

--- client/src/components/FanArt.jsx
+++ client/src/components/FanArt.jsx
@@ -1,7 +1,15 @@
 import React from "react";
-
+import "../style/Fan.css"
 const Fan_art = () =>{
-    return <p>hiu</p>
+    return (
+        <div className="artPage">
+            <div className="changePage">
+                <button className="page">Previous</button>
+                <div className="pageN">1 2 3 ...</div>
+                <button className="page">Next</button>
+            </div>
+        </div>
+    )
 }
 
 export default Fan_art
