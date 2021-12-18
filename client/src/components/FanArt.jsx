37b1f25bae664ff61c74ad84377983a10aaf0e75
Mohamed <31968872+Mohamedkone@users.noreply.github.com>
2021-12-18 11:56:56 -0500

    fan container and card set up in react

--- client/src/components/FanArt.jsx
+++ client/src/components/FanArt.jsx
@@ -1,12 +1,29 @@
 import React from "react";
 import "../style/Fan.css"
+import Thumbnail from "../assets/kimetsu.jpg"
 const Fan_art = () =>{
+    
+    let fanCard = []
+    for(let i = 0; i<20; i++){
+        fanCard.push(<div className="fan-card">
+            <img src={Thumbnail} alt="" />
+            <div className="card-info">
+                <h3>Lorem, ipsum.</h3>
+                <h4 className="author">Lorem, ipsum.</h4>
+            </div>
+        </div>)
+    }
+
+
     return (
         <div className="artPage">
             <div className="changePage">
                 <button className="page">Previous</button>
                 <div className="pageN">1 2 3 ...</div>
                 <button className="page">Next</button>
+            </div>
+            <div className="fanConatiner">
+                {fanCard}
             </div>
         </div>
     )
