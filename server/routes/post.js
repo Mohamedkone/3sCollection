d057908595c753877855c203cfd46dae594e2dd9
Mohamed <31968872+Mohamedkone@users.noreply.github.com>
2021-12-18 17:09:37 -0500

    set up bases for controller

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,9 +1,8 @@
 import express from 'express'
+import { getPosts } from '../controllers/post.js'
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.send('It work')
-})
+router.get('/', getPosts)
 
 export default router
