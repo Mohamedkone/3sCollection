fbf2b97c4a92c21fb238f8d1ca81b3f090914bf2
Mohamed <31968872+Mohamedkone@users.noreply.github.com>
2021-12-18 13:45:08 -0500

    community page ver. 0.1 finished

--- client/src/components/Community.jsx
+++ client/src/components/Community.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import "../style/Community.css"
 
 const Community = () =>{
-    return  <p>prog</p>
+    return  <div className="comingSoon">
+        <h1>Coming Soon...</h1>
+    </div>
 }
 
 export default Community
