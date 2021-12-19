617c4d982c05c7625f3b27594a203599abf4c806
Mohamed <31968872+Mohamedkone@users.noreply.github.com>
2021-12-18 20:59:35 -0500

    added scans

--- client/src/components/Read.jsx
+++ client/src/components/Read.jsx
@@ -1,7 +1,19 @@
 import React from "react";
-
 const Read = () =>{
-    return
+
+
+    return (
+        <div className="readContainer">
+            <div className="readNav">
+
+            </div>
+            <div className="imgContainer">
+                <div className="scan">
+                    
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Read
